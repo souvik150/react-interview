7d8f920dda13db8aee6048479e2cc78dadf40a22
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-12-03 19:45:20 +0530

    feat: intial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+        <div className="text-center">
+          <p className="">Hello world</p>
+        </div>
+  );
+}
+
+export default App;
